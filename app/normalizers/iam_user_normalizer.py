9e4595fc8904292b028668d628b4f2f7d57f0110
jbbok89 <jbbok89@gmail.com>
2026-02-20 15:14:41 +0900

    Update iam_user_normalizer.py

--- app/normalizers/iam_user_normalizer.py
+++ app/normalizers/iam_user_normalizer.py
@@ -7,12 +7,15 @@ def normalize_iam_users(raw_payload: Dict[str, Any], account_id: str, region="gl
 
     for user_value in users:
         name = user_value.get("UserName")
+        resource_id = user_value.get("UserId")
         
         node_type = "iam_user"
-        node_id = f"{account_id}:{node_type}:{name}"
-        resource_id = user_value.get("UserId")
+        node_id = f"{account_id}:{node_type}:{resource_id}"
         arn = user_value.get("Arn")
         create_date = user_value.get("CreateDate")
+        tags = user_value.get("Tags", [])
+        mfa_devices = user_value.get("MFADevices", [])
+        access_keys = user_value.get("AccessKeys", [])
         attached_policies = user_value.get("AttachedPolicies", [])
         inline_policies = user_value.get("InlinePolicies", [])
         group_policies = user_value.get("Groups", [])
@@ -27,6 +30,9 @@ def normalize_iam_users(raw_payload: Dict[str, Any], account_id: str, region="gl
             "attributes": {
                 "arn": arn,
                 "create_date": create_date.isoformat(),
+                "tags": tags,              
+                "mfa_devices": mfa_devices, 
+                "access_keys": access_keys,
                 "attached_policies": attached_policies,
                 "inline_policies": inline_policies,
                 "group_policies": group_policies
