28ca43f76a2c07eb3f82f854eb4b784586badcba
jbbok89 <jbbok89@gmail.com>
2026-02-20 15:12:40 +0900

    Update ec2_normalizer.py

--- app/normalizers/ec2_normalizer.py
+++ app/normalizers/ec2_normalizer.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 from typing import Any, Dict
 from datetime import timezone
+import re 
 
 def normalize_ec2(raw_payload: Dict[str, Any], account_id: str, region: str) -> Dict[str, Any]:
     #Node 생성
     instances = raw_payload.get("instances", [])
     nodes = []
+
     for instance_value in instances:
         instance_id = instance_value.get("InstanceId")
         tags = instance_value.get("Tags", [])
@@ -13,8 +15,7 @@ def normalize_ec2(raw_payload: Dict[str, Any], account_id: str, region: str) -> 
         
         #각 필드를 채우기 위한 값
         node_type = "ec2_instance"
-        node_id = f"{account_id}:{region}:ec2:{instance_id}"
-        #resoucre id == instance id
+        node_id = f"{account_id}:{region}:ec2_instance:{instance_id}"
         name = next((tag['Value'] for tag in tags if tag['Key'] == 'Name'), None)
         instance_type = instance_value.get("InstanceType")
         state = instance_value.get("State").get("Name")
@@ -24,8 +25,21 @@ def normalize_ec2(raw_payload: Dict[str, Any], account_id: str, region: str) -> 
         vpc_id = instance_value.get("VpcId")
         subnet_id = instance_value.get("SubnetId")
         key_name = instance_value.get("KeyName")
-        instance_profile = (instance_value.get("IamInstanceProfile") or {}).get("Arn")
-        security_group = network_interfaces[0].get("Groups", [])
+        #(수정) instance profile 이름만 추출하도록 
+        instance_profile_arn = (instance_value.get("IamInstanceProfile") or {}).get("Arn") #instance profile 수집
+        instance_profile = instance_profile_arn.split('/')[-1] if instance_profile_arn else None #instance profile arn에서 이름만 추출
+        network_interfaces = instance_value.get("NetworkInterfaces", []) #ec2에 연결된 네트워크 인터페이스 목록 가져옴
+
+        security_groups = [ #보안 그룹 수집
+            sg
+            for nic in network_interfaces
+            for sg in nic.get("Groups", [])
+        ]
+
+        #(수정) 필드 추가
+        imds_token = (instance_value.get("MetadataOptions") or {}).get("HttpTokens") #IMDSv2 토큰 설정 추출
+        ecs_cluster = (instance_value.get("EcsClusterName"))
+        
 
         node = {
             "node_type": node_type,
@@ -45,7 +59,11 @@ def normalize_ec2(raw_payload: Dict[str, Any], account_id: str, region: str) -> 
                 "subnet_id": subnet_id,
                 "key_name": key_name,
                 "iam_instance_profile": instance_profile,
-                "security_groups": security_group
+                "security_groups": security_groups,
+                #(수정) 새 필드 추가
+                "imds_token": imds_token,
+                "ecs_cluster": ecs_cluster,
+                "tags": tags
             }
         }
         nodes.append(node)
